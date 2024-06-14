ea5ac7b634b3f91fcece6d0483f7ef177e2a7690
Pranav Bhinge <136009215+Pranav-Bhinge@users.noreply.github.com>
2024-06-14 19:09:19 +0000

    basics of java datatypes typecasting comments syntax

--- Basics/basics_1.java
+++ Basics/basics_1.java
@@ -1,5 +1,28 @@
 public class basics_1{
     public static void main(String args[]){
-        System.out.println("Hello It's my first program");
+        System.out.println("Hello It's my first Program");
+
+        //basic operation in Java
+        System.out.println("Lets see about Data Types");
+        //int myint=20;                        Integer(Whole Number)
+       // float myfloat=3.088F;                Floating number(Decimal)
+        //char mychar='B';                     Character
+    // boolean Mybool="True";                   boolean  {true/False}
+        String mystring="Pranav Bhinge";       //String 
+        System.out.println(mystring);
+
+        //Type Casting
+        System.out.println("Lets see TypeCasting");
+       // int mynum=10;
+        //float myfloat=mynum;
+        //System.out.println(mynum);
+        //System.out.println(myfloat);
+
+        //lets see real life example of it
+        System.out.println("Real life example of type casting");
+        int maxmarks=500;
+        int usermarks=456;
+        float percentage=(float) usermarks/maxmarks*100.00F;
+        System.out.println(percentage); 
     }
 }

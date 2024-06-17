0cfffe42c2727e405ded2f6e25fbcef8068bd5e9
Pranav Bhinge <136009215+Pranav-Bhinge@users.noreply.github.com>
2024-06-17 15:06:56 +0000

    for and while loops

--- Basics/basics_loops.java
+++ Basics/basics_loops.java
@@ -9,9 +9,30 @@ public class basics_loops {
         System.out.println("******Nested for loop*******");
         for (int i = 1; i <=5; i++) {
         for (int j = 1; j <=i; j++) {
-            System.out.println("* ");
+            System.out.print("* ");
         }
             System.out.println();
         }
-    }
+    
+    //reverse order
+    System.out.println("******reverse order loop*******");
+
+    int term=6;
+    for (int i = 1; i <=term; i++) {                //here we have to take variable we cannot give direct i<=5and j=i 
+                                                    //cause we have initialize i with 1 we have to reverse it so wehave to start with max which is 6 here
+        for (int j = term; j >=i; j--) {
+            System.out.print("* ");
+        }
+            System.out.println();
+        }
+    
+        //while loop 
+        System.out.println("****** While Loop *******");
+        int i=0;
+        while(i<=10){
+            System.out.println(i);
+            i++;
+        }
+
+}
 }

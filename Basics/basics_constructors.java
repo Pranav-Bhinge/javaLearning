4da8e85dff3ab1af2a835f4768fde0394a6afb79
Pranav Bhinge <136009215+Pranav-Bhinge@users.noreply.github.com>
2024-06-22 14:37:11 +0000

    constructor default/parameterized

--- Basics/basics_constructors.java
+++ Basics/basics_constructors.java
@@ -1,31 +1,32 @@
 class constructor{
-    constructor(){
+    constructor(){                                            //create default constructor
         System.out.println("normal/default constructor");
     }
 }
 
 class parameterizedconstructor{
-    String Firstname;
+    String Firstname;                                   //paramter for constructor
     String Lastname;
- parameterizedconstructor(String f,String l){
+ parameterizedconstructor(String f,String l){           //creating prameterized constructor
     Firstname=f;
     Lastname=l;
+    
  }
  void show(){
-    System.out.println(Firstname+" "+Lastname);
+    System.out.println(Firstname+" "+Lastname);         // creating method
  }
 
 }
 public class basics_constructors {
     public static void main(String args[]){
             System.out.println("********constructor*********");
-            constructor con=new constructor(); 
+            constructor con=new constructor();                              //creating a object is only thing needed to call constructor
         
             System.out.println("********Parameterized constructor*********");
-            parameterizedconstructor prcon1=new parameterizedconstructor("Pranav", "Bhinge");
+            parameterizedconstructor prcon1=new parameterizedconstructor("Pranav", "Bhinge");    //creating object and giing parameter for parameterized constructor
             parameterizedconstructor prcon2=new parameterizedconstructor("Raman", "Sangamnor");
 
-            prcon1.show();
+            prcon1.show();      // calling method
             prcon2.show();
         
         }

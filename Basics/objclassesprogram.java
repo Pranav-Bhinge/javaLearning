991f64423c7c5132c82afb3f715bac94a68f1c1c
Pranav Bhinge <136009215+Pranav-Bhinge@users.noreply.github.com>
2024-06-22 14:01:50 +0000

    object and classes preogram

--- Basics/objclassesprogram.java
+++ Basics/objclassesprogram.java
@@ -1,17 +1,18 @@
 import java.util.Scanner;
 
 class circle{
-    public static void area(){
-        Scanner s=new Scanner(System.in);
-        System.out.println("Enter the radius");
-        int radius=s.nextInt();
-        float area= 3.14f*radius*radius;
-        System.out.println("Area of circle withv radius"+" "+radius+" "+"is"+" "+area);
+    public float area(int r){
+        float area= 3.14f*r*r;
+        System.out.println("Area of circle withv radius"+" "+r+" "+"is"+" "+area);
+        return area;
     }
 }
 public class objclassesprogram {
     public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the radius");
+        int r=s.nextInt();
         circle c1=new circle();
-        c1.area();
+        c1.area(r);
     }
 }

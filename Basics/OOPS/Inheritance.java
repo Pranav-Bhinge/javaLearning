c8a8a0b2ad86f18f278edaa58c754bbda986b1d7
Pranav Bhinge <136009215+Pranav-Bhinge@users.noreply.github.com>
2024-06-25 19:37:51 +0000

    fd

--- Basics/OOPS/Inheritance.java
+++ Basics/OOPS/Inheritance.java
@@ -1,4 +1,5 @@
 package OOPS;
+//*********************************Single Inheritance******************************* */
 class Animal{      //Parent class
     void eat(){System.out.println("Eating");}
 }
@@ -8,6 +9,8 @@ class Dog extends Animal{     //child class                       //here we use 
 public class Inheritance {
     public static void main (String args[]){
         System.out.println("************Inheritance*********");
+        System.out.println("************Single Inheritance*********");
+
         Dog d1=new Dog();        //created refrence variable/obj
         d1.bark();                 //called method from its own class
          d1.eat();                //called method from parent class
